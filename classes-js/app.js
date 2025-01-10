05be14ad5baeb6406e87c56118aa60d86564d462
KenaiAngel <165106184+KenaiAngel@users.noreply.github.com>
2025-01-09 22:34:11 -0600

    Add methods to the superclass Phone named: restart Phone, showPhone, takePicture, and record. Create: A child class HighTechPhone that inherits from Phone, using the same methods from the superclass while adding two new methods: recordSlowMotion and facialRecognition. Override the showPhone method to provide a custom implementation. Change: The camera attribute is now a static value. The Phone class will have one camera, while the HighTechPhone class will have two cameras.

--- classes-js/app.js
+++ classes-js/app.js
@@ -8,26 +8,22 @@ class phone {
     color;
     weight;
     displayResolution;
-    camara;
+    camara = 1 ;
     ram;
     phoneStatus = false;
 
-    constructor (name, color, weight, displayResolution, camara, ram){
+    constructor (name, color, weight, displayResolution, ram){
         this.name = name;
         this.color = color;
         this.weight = weight;
         this.displayResolution = displayResolution;
-        this.camara = camara;
         this.ram = ram;
     }
 
     set setPhoneStatus(phoneStatus){
-        if( toString.call(phoneStatus) === '[object Boolean]'){
-            this.phoneStatus = phoneStatus;
-        }
-        else{
-            alert(`Error: Value ${phoneStatus} its not a boolean`);
-        }
+        (toString.call(phoneStatus) === '[object Boolean]') 
+            ? (this.phoneStatus = phoneStatus) : console.log(`Error: Value ${phoneStatus} its not a boolean`)
+        ;
         
     }
 
@@ -35,30 +31,86 @@ class phone {
 
     turnOn () {
         
-        if(!this.getPhoneStatus){
-            this.setPhoneStatus = true;
-            alert(`${this.name} on`);
-            
-        }
-        else{
-            alert(`The phone ${this.name} its already on`);
-        }
+        (!this.getPhoneStatus)
+            ? (this.setPhoneStatus = true,console.log(`${this.name} on`))
+            : console.log(`The phone ${this.name} its already on`)
+        ;
+
+    }
+
+    turnOff (){
+        (this.getPhoneStatus) 
+            ? (this.setPhoneStatus = false, console.log(`${this.name} off`))
+            : console.log(`The phone ${this.name} its already off`)
+        ;
+
+    }
+
+    restartPhone (){
+        this.setPhoneStatus = false;
+        console.log(`Restarting ${this.name}`);
     }
 
     showPhone () {
-        console.log(`${this.name}, ${this.color}, ${this.weight}, ${this.displayResolution}, 
-        ${this.camara}, ${this.ram}`)
-        ;
+        console.log(`${this.name}, ${this.color}, ${this.weight}, ${this.displayResolution}, ${this.camara}, ${this.ram}`);
 
-        console.log(this.getPhoneStatus);
+        (this.getPhoneStatus) ? console.log("This phone is off") : console.log("This phone is off");
     }
 
+    takePicture(){
+        (this.getPhoneStatus) ? console.log(`${this.name} have taken a photo`) : console.log(`${this.name} can not take photo without being on`);
+    }
 
+    record(){
+        (this.getPhoneStatus) ? console.log(`${this.name}  had recorded a video`) : console.log(`${this.name} can not take record  without being on`);
+    }
 }
 
-const celu1 = new phone("sam","black",200,"500px",1, 4);
-celu1.showPhone();
-celu1.turnOn();
-celu1.showPhone();
-celu1.turnOn();
+class highTechPhone extends phone {
+    constructor (name, color, weight, displayResolution, ram){
+        super(name, color, weight, displayResolution, ram);
+        this.camara = 2;
+        this.face = "me";
+    }
 
+    showPhone(){
+        console.log("High-Tech phone details:")
+        super.showPhone();
+    }
+
+    recordSlowMotion(){
+        (this.getPhoneStatus) 
+            ? (console.log(`${this.name} had recorded a slow-motion video`)) : (console.log(`${this.name} needs to be on to record`))
+        ;
+    }
+    
+    facialRecognition(){
+        if (this.getPhoneStatus){
+            let input = prompt(`Face for ${this.name}`);
+            (input === this.face) 
+                ? (console.log(`Welcome back ${this.face}`), this.setPhoneStatus = true)
+                : (console.log(`Try again`), this.facialRecognition())
+            ;
+
+        }else{
+            alert(`This function is not availiable while the phone is off`);
+        }
+        
+    }
+}
+
+const celu1 = new phone("Iphone","black",200,"500px", 4);
+celu1.showPhone();
+celu1.turnOn();
+celu1.showPhone();
+celu1.turnOff();
+celu1.showPhone();
+celu1.takePicture();
+celu1.turnOn();
+celu1.record();
+
+const celu2 = new highTechPhone("Samsung","black",200,"500px", 4);
+celu2.showPhone();
+celu2.turnOn();
+celu2.recordSlowMotion();
+celu2.facialRecognition();
